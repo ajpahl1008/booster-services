d0bce6a344ab134995ed4df5b2af75029773040a
AJ Pahl <ajpahl1008@gmail.com>
2016-07-29 22:33:10 -0400

    Added missing Maven Dependency for JUNIT

--- src/test/java/BoosterServiceRequestTest.java
+++ src/test/java/BoosterServiceRequestTest.java
@@ -1,11 +1,11 @@
 import com.pahlsoft.booster.BoosterRequestService;
-
 import junit.framework.Assert;
 import org.junit.Test;
 
 public class BoosterServiceRequestTest {
 
-    @Test() public void retrieveListOf9Items() {
+    @Test
+    public void retrieveListOf9Items() {
         BoosterRequestService boosterRequestService = new BoosterRequestService();
         Assert.assertEquals(boosterRequestService.retrieveServerList().size(), 10);
     }
